c0327bc3b9f7aedd4647efa82fddc90278158b81
Herman Odilov <german.dark00@gmail.com>
2019-04-14 19:30:38 +0700

    added tester and fitness function

--- Mut.cpp
+++ Mut.cpp
@@ -1,26 +1,101 @@
 #include "Mut_Del.cpp"
+#include "Mut_add.cpp"
+#include "Cross.cpp"
+#include "Const.cpp"
 #include <ctime>
 
 int main()
 {
+	freopen("Code.cpp", "w", stdout);
 	srand(time(0));
-	GenCode A;
-	A.Gens={7,10,8,3,2,3,4,9,3,2,3,4,11,3,5,3,6,3,7,15,3,8,3,9};
-	long y;
-	cout<<A.comp(0,y)<<" ";
-	cout<<y<<endl;
-	Mut_Del(A);
-	long cur=0;
-/*	while (cur<A.Gens.size())
+	GenCode A,B,R;
+	long b=0;
+	long n=40;
+	while (n>0)
 	{
-		cur=A.comp(cur,y);
-		cout<<endl<<cur<<" ";
-		cur++;
-	}*/
-	cur=0;
-	while (cur<A.Gens.size())
-	{
-		cout<<A.Gens[cur]<<" ";
-		cur++;
+		add(A);
+		n--;
 	}
+	if (b!=0)
+	{
+		A.Gens=A.Gens+"B";
+	}
+	b=0;
+	n=40;
+	while (n>0)
+	{
+		add(B);
+		n--;
+	}
+	if (b!=0)
+	{
+		B.Gens=B.Gens+"B";
+	}
+
+	R=cross(A,B);
+	cout<<endl;
+	n=3;
+	while (n>0)
+	{
+		Mut_all(R);
+		n--;
+	}
+	cout<<endl;
+	rout(R);
+}
+
+unsigned long long tester(
+	const char * pfile,
+	const char * ofile,
+	const char * ifile,
+	const std::string & prog,
+	const std::vector<std::vector<long long>> & a,
+	const std::vector<unsigned long long> & x
+)
+{
+	std::ofstream pstream(pfile);
+	
+	pstream << prog << '\n';
+	
+	system("gnome-terminal -x sh -c 'g++ -std=c++17 -o main *.cpp'");
+	
+	char check[(int)1e9];
+	std::vector<unsigned long long> y(a.size());
+	
+	unsigned long long unic_ans = 0;
+	unsigned long long r_ans = 0;
+	
+	for(unsigned long long i = 0; i < a.size(); ++i)
+	{
+		std::ifstream ostream(ofile);
+		std::ofstream istream(ifile);
+		
+		istream << a[i].size() << '\n';
+		for(unsigned long long j = 0; j < a[i].size(); ++i)
+		{
+			istream << a[i][j] << ' ';
+		}
+		
+		system("gnome-terminal -x sh -c './main'");
+		
+		ostream >> y[i];
+		
+		unic_ans += check[y[i]];
+		r_ans += y[i] == x[i];
+		
+		check[y[i]] = true;
+	}
+	
+	return r_ans + unic_ans;
+}
+
+unsigned long long fitness(
+	const char * pfile,
+	const char * ofile,
+	const std::string & prog,
+	const std::vector<std::vector<long long>> & a,
+	const std::vector<unsigned long long> & x
+)
+{
+	return tester(pfile, ofile, prog, a, x);
 }
--- gef.cpp
+++ gef.cpp
@@ -1,46 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 struct GenCode
 {
-	vector<long>Gens;
+	string Gens="";
 	long comp(long compb,long &y)
 	{
-		if (compb==Gens.size())
+		if (compb>=Gens.size())
 		{
 			return Gens.size();
 		}
-		if (Gens[compb]==3)
+		if (Gens[compb]=='1')
 		{
 			return compb+1;
 		}
-		if ((Gens[compb]==2)or(Gens[compb]==4)or(Gens[compb]==6)or(Gens[compb]==10)or(Gens[compb]==12)or(Gens[compb]==13))
+		if ((Gens[compb]=='2')or(Gens[compb]=='3')or(Gens[compb]=='4')or(Gens[compb]=='5'))
 		{
-		   	return comp(compb+1,y);																		 
+			return comp(compb+1,y);
 		}
-		if ((Gens[compb]==5)or(Gens[compb]==8)or(Gens[compb]==9))
+		if ((Gens[compb]=='6')or(Gens[compb]=='7')or(Gens[compb]=='8'))
 		{
 			long p=comp(compb+1,y);
 			return comp(p+1,y);
 		}
-		if ((Gens[compb]==7))
+		if (Gens[compb]=='9')
 		{
-			long t=compb;
-			while (Gens[t]!=11)
+			long p=comp(compb+1,y);
+			y=comp(p+1,y);
+			long t=y;
+			while((Gens[t]!='B')and(t<Gens.length()))
 			{
 				t++;
 			}
-			y=t+1;
-			t=comp(t,y);
-			return t;
-		}
-		if (Gens[compb]==11){
-			long t=compb;
-			while (Gens[t]!=15)
+			if (t==Gens.length())
 			{
-				t++;
+				t--;
 			}
 			return t;
 		}
